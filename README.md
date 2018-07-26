70cb1eb3c07f01b8e81b5a045ce1b19a1b326a02
mariaforensa <social.scrapy@yadex.com>
2018-07-26 12:00:51 -0300

    data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cms
+#settings
+
+
--- cms_scrap/cms_scrap/settings.py
+++ cms_scrap/cms_scrap/settings.py
@@ -19,7 +19,8 @@ NEWSPIDER_MODULE = 'cms_scrap.spiders'
 #USER_AGENT = 'cms_scrap (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +28,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
